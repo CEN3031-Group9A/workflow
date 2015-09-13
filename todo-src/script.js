7f6b68e826a9c4a5be96d254e0e9611c7c4613d0
Nicholas Cioli <nicholascioli@gmail.com>
2015-09-13 15:23:56 -0400

    implement the assignments listed in the readme
    Well, kind of. It doesn't implement the priority thing (mostly
    because I forgot that that was a thing), but it should be pretty
    straightforward. Something as simple as having separate variables
    per priority would even suffice.

--- todo-src/script.js
+++ todo-src/script.js
@@ -5,19 +5,61 @@ var myApp = angular.module('app', []);
 myApp.controller('MainCtrl', function ($scope){
   $scope.todos = ["Learn Angular", "Learn node"];
   $scope.newItem = "";
+  $scope.done = [];
+  $scope.custom_show = false;
+  $scope.editItem = "";
   
-  $scope.addItem = function(){
+  $scope.addItem = function(event){
+    if (!event || event.key !== "Enter")
+      return;
+
     console.log("in add");
     if ($scope.newItem !== ""){
       $scope.todos.push($scope.newItem);
       $scope.newItem = "";
     }
   }
-    
+  
+  $scope.completeItem = function(item) {
+    var index = $scope.todos.indexOf(item);
+    $scope.todos.splice(index, 1);
+    $scope.done.push(item);
+  }
+
+  $scope.startEdit = function (item) {
+    this.custom_show = item;
+    this.editItem = item;
+  }
+
+  $scope.doEditItem = function (keypress) {
+    if (!keypress || keypress.key !== "Enter")
+      return;
+
+    if (this.editItem !== "")
+    {
+      var index = $scope.todos.indexOf(this.do);
+
+      $scope.todos[index] = this.editItem;
+      this.editItem = "";
+      this.custom_show = false;
+    }
+  }
+
   $scope.deleteItem = function(item){
     console.log("in delete");
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
+  }
+
+  $scope.completeAll = function(){
+    for (item in $scope.todos)
+      $scope.done.push($scope.todos[item]);
+
+    $scope.clear();
+  }
+
+  $scope.clear = function() {
+    $scope.todos = [];
   }
     
   
