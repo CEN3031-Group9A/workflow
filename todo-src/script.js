fc4c381ace756eeb2146583b8cc0d05311ed010d
Chris James <TobalJackson@gmail.com>
2015-09-13 16:36:03 -0400

    started work

--- todo-src/script.js
+++ todo-src/script.js
@@ -5,6 +5,8 @@ var myApp = angular.module('app', []);
 myApp.controller('MainCtrl', function ($scope){
   $scope.todos = ["Learn Angular", "Learn node"];
   $scope.newItem = "";
+  $scope.visible = true;
+  $scope.inEditMode = false;
   
   $scope.addItem = function(){
     console.log("in add");
@@ -18,6 +20,22 @@ myApp.controller('MainCtrl', function ($scope){
     console.log("in delete");
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
+  }
+
+  $scope.editItem = function(item){
+    console.log("in edit");
+    var index = $scope.todos.indexOf(item);
+
+	if (this.visible === true){
+	    if (this.inEditMode === true){
+	  	  this.visible = false;
+		  this.inEditMode = true;
+	    }
+	}
+  }
+
+  $scope.isDisplayed = function(li){
+	  return this.visible;
   }
     
   
