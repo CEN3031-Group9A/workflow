b4b4b257a49797422abafeeda7c9abb3d17401b0
Chris James <tobaljackson@gmail.com>
2015-09-14 23:15:06 -0400

    enabled edit mode. confirm with enter or button

--- todo-src/script.js
+++ todo-src/script.js
@@ -7,6 +7,7 @@ myApp.controller('MainCtrl', function ($scope){
   $scope.newItem = "";
   $scope.visible = true;
   $scope.inEditMode = false;
+  $scope.editItem = "";
   
   $scope.addItem = function(){
     console.log("in add");
@@ -24,8 +25,40 @@ myApp.controller('MainCtrl', function ($scope){
 
   $scope.enableEditMode = function(item){
 	this.inEditMode = true;
+//	this.visible = !this.visible;
   }
 
+  $scope.isDisplayed = function(li){
+	  return this.visible;
+  }
+  $scope.confirmEdit = function(item){
+	  console.log(this.editItem);
+	  var index = $scope.todos.indexOf(item);
+	  $scope.todos[index] = this.editItem;
+	  this.editItem = "";
+	  this.inEditMode = false;
+
+  }
+
+  $scope.cancelEdit = function(item){
+	  this.inEditMode = false;
+	  this.editItem = "";
+  }
+
+
+  $scope.keyConfirmEdit = function(event){
+	  if (event && !$scope.validate(event, 13))
+		  return
+	  var index = $scope.todos.indexOf(this.do);
+	  $scope.todos[index] = this.editItem;
+	  this.editItem = "";
+	  this.inEditMode = false;
+
+  }
+  $scope.validate = function(event, keycode){
+      var key = event.charCode ? event.charCode : event.keyCode ? event.keyCode : 0
+      return key === keycode;
+  }
 //  $scope.editItem = function(item){
 //    console.log("in edit");
 //    var index = $scope.todos.indexOf(item);
@@ -38,9 +71,6 @@ myApp.controller('MainCtrl', function ($scope){
 //	}
 //  }
 
-  $scope.isDisplayed = function(li){
-	  return this.visible;
-  }
     
   
 });
